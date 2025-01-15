cccd5e02c7a9a575841bbc730f89faa392ac25fd
Felipe Carvalho <felipe_pc7@hotmail.com>
2025-01-15 19:43:39 -0300

    feat: list url history & redirect short url to original

--- src/controllers/links-controller.ts
+++ src/controllers/links-controller.ts
@@ -11,15 +11,49 @@ export class LinksController {
             return
         }
 
-        const shortUrl = nanoid(8)
+        try {
+            const shortUrl = nanoid(8)
 
-        const newShortUrl = linksRepository.create({
-            originalUrl,
-            shortUrl
-        })
+            const newShortUrl = linksRepository.create({
+                originalUrl,
+                shortUrl
+            })
 
-        await linksRepository.save(newShortUrl)
+            await linksRepository.save(newShortUrl)
 
-        res.json(newShortUrl)
+            res.json(newShortUrl)
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({message: 'Internal server error'})
+        }
+    }
+
+    async redirectShortUrl(req: Request, res: Response) {
+        const { shortUrl } = req.params
+
+        const url = await linksRepository.findBy({ shortUrl })
+
+        if(!url) {
+            res.status(404).json({message: 'url not found'})
+            return
+        }
+
+        try {
+            res.redirect(url[0].originalUrl)
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({message: 'Internal server error'})
+        }
+    }
+
+    async urlHistory(req: Request, res: Response) {
+        try {
+            const urls = await linksRepository.find()
+
+            res.json(urls)
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({message: 'Internal server error'})
+        }
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,4 +3,7 @@ import { LinksController } from './controllers/links-controller'
 
 export const linksRouter = Router()
 
+
 linksRouter.post('/url', new LinksController().shortenUrl)
+linksRouter.get('/url/:shortUrl', new LinksController().redirectShortUrl)
+linksRouter.get('/url', new LinksController().urlHistory)
